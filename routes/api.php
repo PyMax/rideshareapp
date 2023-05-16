ce0602639ab80a7b9c630b8dd85186903b1c64d2
PyMax <qtcreator777@gmail.com>
2023-05-16 18:30:53 +0300

    added trip driver logic

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\DriverController;
+use App\Http\Controllers\TripController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,3 +20,16 @@ use App\Http\Controllers\LoginController;
 Route::post('/login', [LoginController::class, 'submit']);
 
 Route::post('/login/verify', [LoginController::class, 'verify']);
+
+
+Route::group(['middleware' => 'auth:sanctum'], function(){
+    Route::get('driver', [DriverController::class, 'show']);
+    Route::post('driver', [DriverController::class, 'update']);
+
+    Route::post('/trip', [TripController::class, 'store']);
+    Route::get('/trip/{trip}',[TripController::class, 'show']);
+
+    Route::get('/user', function (Request $request){
+        return $request->user();
+    });
+});
